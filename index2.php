128af6eb13a056c3d2a99fb48186942359b16c6b
flo003lol <itsflo3@gmail.com>
2024-01-21 20:06:06 +0100

    auth

---
+++ index2.php
@@ -0,0 +1,35 @@
+<?php
+include('auth.php');
+session_start();
+
+if (!isset($_SESSION['authenticated']) || !$_SESSION['authenticated']) {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if (isset($users[$username]) && password_verify($password, $users[$username])) {
+            $_SESSION['authenticated'] = true;
+        } else {
+            // Invalid login attempt, handle accordingly
+            header('Location: login.html'); // Redirect to login page
+            exit();
+        }
+    } else {
+        // User is not logged in, redirect to login page
+        header('Location: login.html');
+        exit();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
